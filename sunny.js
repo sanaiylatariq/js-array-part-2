b538bc9d6867e06a2a9dbcc1c312deb31f3b3d47
sanaiylatariq <sanaiylatariq517@gmail.com>
2024-07-03 11:38:08 +0500

    Add files via upload

---
+++ sunny.js
@@ -0,0 +1,52 @@
+// let mynames = ["sunny", "kato", "bundo"]
+// let othername = ["dani", "khota", "donkey", "dangar"]
+// console.log(mynames);
+// console.log(mynames[3][2]);
+// mynames.push(othername)
+// console.log(mynames);
+// console.log(mynames[3][2]);
+// allnames = mynames.concat(othername)
+// console.log(allnames)
+// console.log(allnames);
+// allnames = mynames.concat(othername)
+// console.log(allnames);
+// let names = ["sunny","sanaiyla", "kato"]
+// let othernames = ["dani", "khota", "donkey"]
+// let allnames = names.concat(othernames)
+// console.log(allnames);
+
+
+// to combine 2 arrays use another var put dot dot dot name of one array , and dot dot dot and the name of other array  this will effectively combine 2 arrays instead of concat jhamaela  
+
+
+// let allnames = [...names,...othernames]
+// console.log(allnames);
+// let sunny2 = [...names,...othernames]
+// console.log(sunny2);
+// const another_array = [0,1,2,[3,4,5,3,[4,5,6,7,],8,9,[10,11,12,34]]
+// console.log(another_array);
+
+//  flat property sb arrays ko join krte hay use a var us m previous array wla var then .flat (infinity)to sb arrays kthay ho jaen gay
+
+
+
+
+// let another_array = [0,1,2,[3,4,5,], 4,5,[6,7,8,[59,40,30,20,]]]
+// let newarray = another_array.flat(Infinity)
+// console.log(newarray);
+
+
+// kisi b chz ko array mn convert karny k lye ap array .from within brackets ap likho kch b chaahy string elemrnt ho wo array mn bdl jaega 
+
+// console.log(Array.from("sunny"));
+// console.log(Array.from(456));
+let score1 = 20
+let score2 = 67
+let score3 = 64
+// let scoreall = [...score1,...score2,...score3]
+// console.log(Array.of(score1,score2,score3));
+// console.log(Array.from(score1));
+// console.log(Array.from(score2));
+// console.log(Array.from(score3));
+// console.log(Array.from(scoreall));
+console.log(Array.of(score1,score2,score3));
